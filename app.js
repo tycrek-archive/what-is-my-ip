9b894dc12a5e4800b17288a6c1b80d02503bfa57
tycrek <josh.moore@jmoore.dev>
2019-10-18 14:15:22 -0600

    Cleanup

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const PORT = 8738;
 
-// IP parsing modes
-const MODE = {
-	default: 0,
-	v4: 1,
-	v6: 2
-};
-
-// NPM Module imports
-var express = require('express');
-
 // Express app setup
-var app = express();
+var app = require('express')();
 
-// Routes
 app.get('/(:mode)?', (req, res) => {
+	let ip = req.ip;
 	let mode = req.params.mode;
 	let json = mode && mode.includes('json');
 
-	if (!mode) respond(res, parseIp(req.ip));
-	else respond(res, parseIp(req.ip, mode, json));
-	//else respond(res, req.ip);
+	respond(res, mode ? parseIp(ip, mode, json) : parseIp(ip));
 });
-
-//app.use((req, res, _next) => respond(res, parseIp(req.ip)));
 
 // Run Express app
 app.listen(PORT, () => console.log(`Server hosted on port: ${PORT}`));
@@ -38,7 +24,7 @@ function respond(res, payload) {
 const LOCALS = ['localhost', '127.0.0.1', '::1'];
 
 // Parse an IP address
-function parseIp(ip, mode = '0', json) {
+function parseIp(ip, mode = '', json) {
 	if (LOCALS.includes(ip)) ip = 'localhost';
 	else if (mode.startsWith('4')) ip = ip.split(':').pop();
 	else if (mode.startsWith('6')) ip = ip.split(':').slice(0, -1).join(':');
